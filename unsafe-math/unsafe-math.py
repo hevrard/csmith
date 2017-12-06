55b745b1bb89f3d2b6aa8259d1efc5bca3643fd3
Hugues Evrard <h.evrard@imperial.ac.uk>
2017-12-06 22:50:21 +0000

    Rename c file as dyn_safe.c, and various improvements

--- unsafe-math/unsafe-math.py
+++ unsafe-math/unsafe-math.py
@@ -3,10 +3,8 @@
 import sys
 import os
 
-program = sys.argv[1]
-
 UB_ERRFILE = 'ub_error.txt'
-UB_CFILE = 'ub.c'
+DYN_SAFE_FILE = 'dyn_safe.c'
 TIMEOUT = '30'
 
 def makeAllUnsafe(program):
@@ -16,23 +14,29 @@ def makeAllUnsafe(program):
         source = source.replace('safe_', 'UNSAFE_')
     return source
 
+def runCmd(cmd):
+    cmd += ' > _out 2> _err'
+    ret = os.system(cmd)
+    if ret != 0:
+        print('ERROR: the following command returned non-zero value: {}'.format(ret))
+        print(cmd)
+        exit(1)
+    os.system('rm -f _out _err')
+
 def collectUB(program, errFile):
 
-    cmd = 'clang'
+    cmd = 'clang-3.8'
     cmd += ' ' + '-fsanitize=undefined'
     cmd += ' ' + '-I${CSMITH_HOME}/runtime'
     cmd += ' ' + program
-    cmd += ' ' + '> _out 2> _err'
-    ret = os.system(cmd)
-
-    if ret != 0:
-        print('compilation failed')
-        exit(1)
-
+    runCmd(cmd)
+        
     cmd = 'timeout --kill-after=1 ' + TIMEOUT
     cmd += ' ./a.out > /dev/null 2> ' + errFile
     ret = os.system(cmd)
 
+    os.remove('a.out')
+    
     if ret != 0:
         print('timeout')
         exit(124)
@@ -86,35 +90,90 @@ def revertMacros(program, errorLocation):
             f.write(line)
     return nbReverted
 
+def applyUnsafeMath(program):
+    errorLocation = collectUB(program, UB_ERRFILE)
+    if len(errorLocation) > 0:
+        print('Original program has UB!')
+        exit(2)
+
+    print('Apply UNSAFE everywhere')
+    with open(DYN_SAFE_FILE, 'w') as f:
+        f.write(makeAllUnsafe(program))
+
+    i = 0
+    while True:
+        print('# round {}'.format(i))
+
+        errorLocation = collectUB(DYN_SAFE_FILE, UB_ERRFILE)
+
+        nbErr = len(errorLocation)
+        print('UB errors: {}'.format(nbErr))
+
+        if (nbErr == 0):
+            #print('no more UB errors, done')
+            break
+
+        #print('Revert the UB to safe math')
+        errorLocation = correctErrorLocation(errorLocation)
+        nbReverted = revertMacros(DYN_SAFE_FILE, errorLocation)
+        #print('Reverted {} macros'.format(nbReverted))
+        if nbReverted == 0:
+            print('ERROR: cannot revert any macro, reached a buggy fixpoint?')
+            exit(1)
+        i += 1
+
+def outputFromCompiledWith(compiler):
+    return compiler.replace(' -', '_') + '_out'
+        
+def compileAndRun(program, compiler):
+    cmd = compiler + ' -I$CSMITH_HOME/runtime ' + program + ' -o a.out'
+    runCmd(cmd)
+
+    cmd = './a.out > ' + outputFromCompiledWith(compiler)
+    ret = os.system(cmd)
+
+    if ret != 0:
+        print('This command returned {}: {} '.format(ret, cmd))
+        exit(1)
+
+    os.remove('a.out')
+        
 ######################################################################
 
-errorLocation = collectUB(program, UB_ERRFILE)
-if len(errorLocation) > 0:
-    print('Original program has UB!')
-    exit(2)
+seed = sys.argv[1]
 
-print('Apply UNSAFE everywhere')
-with open(UB_CFILE, 'w') as f:
-    f.write(makeAllUnsafe(program))
+#print('Seed: {}'.format(seed))
 
-i = 0
-while True:
-    print('# round {}'.format(i))
+ret = os.system('csmith --seed {} > test.c'.format(seed))
+if ret != 0:
+    print('csmith returns: {}'.format(ret))
+    exit(1)
 
-    errorLocation = collectUB(UB_CFILE, UB_ERRFILE)
+applyUnsafeMath('test.c')
 
-    nbErr = len(errorLocation)
-    print('UB errors: {}'.format(nbErr))
+COMPILERS = [
+    'clang -O0',
+    'clang -O1',
+    'clang -O2',
+    'clang -O3',
+    'clang -Os',
+    'gcc -O0',
+    'gcc -O1',
+    'gcc -O2',
+    'gcc -O3',
+    'gcc -Os'
+]
 
-    if (nbErr == 0):
-        #print('no more UB errors, done')
-        break
+for cc in COMPILERS:
+    compileAndRun(DYN_SAFE_FILE, cc)
 
-    #print('Revert the UB to safe math')
-    errorLocation = correctErrorLocation(errorLocation)
-    nbReverted = revertMacros(UB_CFILE, errorLocation)
-    #print('Reverted {} macros'.format(nbReverted))
-    if nbReverted == 0:
-        print('ERROR: cannot revert any macro, reached a buggy fixpoint?')
-        exit(1)
-    i += 1
+ref = ''
+for i, cc in enumerate(COMPILERS):
+    with open(outputFromCompiledWith(cc)) as f:
+        if i == 0:
+            ref = f.read()
+        else:
+            content = f.read()
+            if ref != content:
+                print('different output for different compilers')
+print('all compilers lead to same output')
