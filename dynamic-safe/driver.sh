47a7540cacd922e322f7e30bb20b2dcc8b9ad69f
Hugues Evrard <h.evrard@imperial.ac.uk>
2017-12-18 15:31:44 +0000

    Add and rename scripts

---
+++ dynamic-safe/driver.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for seed in `seq 4901 14002`
+do
+    echo "======== START $seed ========="
+    num=`printf "%4.4d" $seed`
+    expdir="exp_$num"
+    # rm -rf $expdir
+    # mkdir $expdir
+
+    if test -f $expdir/dynamic_safe.c
+    then
+	(
+	    cd $expdir
+	    ../dynamic-safe.py $seed
+	)
+    else
+	echo "No dynamic_safe.c found, skip"
+    fi
+
+done
